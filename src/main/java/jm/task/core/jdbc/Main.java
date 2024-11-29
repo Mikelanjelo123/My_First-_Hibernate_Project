f63c5bb7d8e7e1eec17303c8bc416971b1f67a43
Mikelanjelo <m.13021999@mail.ru>
2024-11-29 05:17:49 +0300

    ready

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,16 +7,16 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
         UserDao userDao = new UserDaoJDBCImpl();
-      userDao.createUsersTable();
+        userDao.createUsersTable();
 
-//        userDao.saveUser("Name1", "LastName1", (byte) 20);
-//        userDao.saveUser("Name2", "LastName2", (byte) 25);
-//        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
 
-//        userDao.removeUserById(1);
-//        System.out.println(userDao.getAllUsers());
+        userDao.removeUserById(1);
+        System.out.println(userDao.getAllUsers());
         userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
+        userDao.dropUsersTable();
     }
 }
