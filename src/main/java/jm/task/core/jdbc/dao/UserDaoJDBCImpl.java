ca8c5556f76d69d36ef43517953ef662bdbacba8
Mikelanjelo <m.13021999@mail.ru>
2024-11-29 04:58:06 +0300

    ready

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,90 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+import static com.mysql.cj.MysqlType.TINYINT;
 
+public class UserDaoJDBCImpl  implements UserDao {
+    Connection connection = Util.getDBConnection();
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
+        String createUsersTableSQL = """
+                CREATE TABLE IF NOT EXISTS users (
+                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(50),
+                lastName VARCHAR(50),
+                age TINYINT
+                );
+                """;
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(createUsersTableSQL);
+        } catch (SQLException e) {
 
+        }
     }
 
     public void dropUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
 
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String insertUserSQL = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(insertUserSQL)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+        }
     }
 
     public void removeUserById(long id) {
+        String deleteUserSQL = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(deleteUserSQL)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
 
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String selectAllUsersSQL = "SELECT * FROM users";
+        List<User> users = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(selectAllUsersSQL)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                users.add(new User(name, lastName, age));
+            }
+        } catch (SQLException e) {
+
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String truncateUsersTableSQL = "TRUNCATE TABLE users";
+        try (PreparedStatement statement = connection.prepareStatement(truncateUsersTableSQL)) {
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/kata";
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "123456";
+
+
+    static public Connection getDBConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return connection;
+    }
 }
